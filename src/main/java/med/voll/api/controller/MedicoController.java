816c8f5c4dc7041fb64b8af0c883963bf4fdec47
gabriel.fonseca-de <gabriel.fonseca.on@gmail.com>
2024-01-16 22:14:41 -0300

    Creating my constructors and interface

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -31,4 +31,10 @@ public class MedicoController {
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosCadastroMedico dados) {
+
+    }
+
 }
